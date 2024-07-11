b0e66d2735f62eb4193ca48c9097c5164c29e6e5
alianlbj23 <alianlbj23@gmail.com>
2024-07-11 11:37:12 +0800

    new version publish

--- car_publish.py
+++ car_publish.py
@@ -1,42 +1,26 @@
-import asyncio
-import websockets
-import json
+"""
+created_at_utc  : 2024-07-01T02:39:25Z
+created_at_w3c  : 2024-07-01T10:39:25+08:00
+PROS-Blocks     : 0.0.2
+"""
+
+import roslibpy
 import orjson
+import time
 
-DEVICE_DATA_TYPE_ENUM = {"CAR_B_CONTROL": "car_B_control"}
+ros_client = roslibpy.Ros(host="localhost", port=9090)
+ros_client.run()
+
+car_control_topic = roslibpy.Topic(ros_client, "car_B_control", "std_msgs/String")
 
 
-def create_car_b_control(target_vel):
-    return {"target_vel": target_vel}
-
-
-async def advertise_topic(websocket):
-    advertise_message = {
-        "op": "advertise",
-        "topic": DEVICE_DATA_TYPE_ENUM["CAR_B_CONTROL"],
-        "type": "std_msgs/String",
+def publish_to_writer(left_wheel_value, right_wheel_value):
+    control_signal = {
+        "type": "car_B_control",
+        "data": {"target_vel": [left_wheel_value, right_wheel_value]},
     }
-    await websocket.send(json.dumps(advertise_message))
-    print(f"Advertised topic: {advertise_message}")
-
-
-async def publish_to_writer(left_wheel_value, right_wheel_value):
-    uri = "ws://192.168.0.210:9090"
-
-    async with websockets.connect(uri) as websocket:
-        await advertise_topic(websocket)
-
-        control_signal = {
-            "type": DEVICE_DATA_TYPE_ENUM["CAR_B_CONTROL"],
-            "data": create_car_b_control([left_wheel_value, right_wheel_value]),
-        }
-        control_msg = {
-            "op": "publish",
-            "topic": DEVICE_DATA_TYPE_ENUM["CAR_B_CONTROL"],
-            "msg": {"data": orjson.dumps(control_signal).decode()},
-        }
-        await websocket.send(json.dumps(control_msg))
-        print(f"Message sent: {control_msg}")
+    control_msg = {"data": orjson.dumps(control_signal).decode()}
+    car_control_topic.publish(control_msg)
 
 
 def value_ratio(value):
@@ -45,51 +29,12 @@ def value_ratio(value):
     mapped_value = new_min + (value - old_min) * (new_max - new_min) / (
         old_max - old_min
     )
-    print(mapped_value)
     return mapped_value
 
 
 def set_two_wheel(left_wheel_value, right_wheel_value):
-    asyncio.get_event_loop().run_until_complete(
-        publish_to_writer(value_ratio(left_wheel_value), value_ratio(right_wheel_value))
-    )
+    publish_to_writer(value_ratio(left_wheel_value), value_ratio(right_wheel_value))
+    time.sleep(0.1)
 
 
-def car_customize_wheel_value(left_wheel_value=0, right_wheel_value=0):
-    set_two_wheel(left_wheel_value, right_wheel_value)
-
-
-def car_forward():
-    set_two_wheel(5, 5)
-
-
-def car_back():
-    set_two_wheel(-5, -5)
-
-
-def car_counterclockwise_rotate():
-    set_two_wheel(-5, 5)
-
-
-def car_clockwise_rotate():
-    set_two_wheel(5, -5)
-
-
-def car_left_lean():
-    set_two_wheel(2, 5)
-
-
-def car_right_lean():
-    set_two_wheel(5, 2)
-
-
-def car_stop():
-    set_two_wheel(0, 0)
-
-
-def main(args=None):
-    car_customize_wheel_value(0, 0)
-
-
-if __name__ == "__main__":
-    main()
+set_two_wheel(5, 5)
