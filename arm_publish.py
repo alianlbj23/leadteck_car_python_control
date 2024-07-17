cec6dc416f8b35b61258c6f501f6b3e87f2448bd
alianlbj23 <alianlbj23@gmail.com>
2024-07-17 10:15:55 +0800

    [FEAT] Add just change one axis

--- arm_publish.py
+++ arm_publish.py
@@ -6,10 +6,25 @@ import time
 ros_client = roslibpy.Ros(host="192.168.0.210", port=9090)
 ros_client.run()
 
-# 定义话题
+# 定义发布和订阅的话题
 car_control_topic = roslibpy.Topic(
     ros_client, "/robot_arm", "trajectory_msgs/JointTrajectoryPoint"
 )
+
+previous_state_topic = roslibpy.Topic(
+    ros_client, "/robot_arm", "trajectory_msgs/JointTrajectoryPoint"
+)
+
+# 初始默认关节状态（假设有6个关节）
+default_positions = [
+    math.radians(90),
+    math.radians(100),
+    math.radians(20),
+    math.radians(80),
+    math.radians(30),
+    0,
+]
+last_positions = default_positions.copy()
 
 
 def publish_to_writer(
@@ -33,10 +48,22 @@ def publish_to_writer(
     }
     print("Publishing:", control_signal)
     car_control_topic.publish(roslibpy.Message(control_signal))
+    time.sleep(1)
 
 
 def degree_to_radian(value_list):
-    return [math.radians(value) for value in value_list]
+    new_list = []
+    for value in value_list:
+        if value == -1:
+            new_list.append(-1)
+        else:
+            new_list.append(math.radians(value))
+    return new_list
+    # return [math.radians(value) for value in value_list if value is not None]
+
+
+def radian_to_degree(value_list):
+    return [math.degrees(value) for value in value_list if value is not None]
 
 
 def reset_robot_arm():
@@ -44,15 +71,39 @@ def reset_robot_arm():
     publish_to_writer(positions=reset_radian)
 
 
+def axis1_arm():
+    new_angle = degree_to_radian([-1, -1, -1, 50, -1, -1])
+    publish_to_writer(positions=new_angle)
+
+
+def state_callback(message):
+    global last_positions
+    last_positions = message["positions"]
+    print("Received previous data:", last_positions)
+
+
 def main():
-    reset_robot_arm()
+    # # 订阅获取上一次发布的关节状态
+    # previous_state_topic.subscribe(state_callback)
+    # print("Subscribed to /robot_arm topic to receive previous data.")
+
+    # # 等待一定时间以确保接收到消息
+    # time.sleep(5)
+
+    # # 打印上一次接收到的信号
+    # print("Last positions:", radian_to_degree(last_positions))
+
+    # # 取消订阅
+    # previous_state_topic.unsubscribe()
+    # reset_robot_arm()
+    axis1_arm()
 
 
 if __name__ == "__main__":
     main()
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        ros_client.terminate()
+    # try:
+    #     while True:
+    #         time.sleep(1)
+    # except KeyboardInterrupt:
+    #     ros_client.terminate()
